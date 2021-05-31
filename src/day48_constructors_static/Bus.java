0672abcbc17691853b4b1d15eeda5affd709d46c
Rovshan Aliyev <raliyev831@yahoo.com>
2021-05-31 12:34:27 -0700

    day 47 finished

--- src/day48_constructors_static/Bus.java
+++ src/day48_constructors_static/Bus.java
@@ -3,4 +3,9 @@ package day48_constructors_static;
 public class Bus {
     Driver driver;
     Engine engine;
+
+    @Override
+    public String toString() {
+        return driver.getName() + " | " + engine.getCylinders();
+    }
 }
---
+++ src/day48_constructors_static/static_examples/CalculatorTest.java
@@ -0,0 +1,13 @@
+package day48_constructors_static.static_examples;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator.add(5, 3);
+
+        Calculator calculator = new Calculator();
+        calculator.multiply(2, 4);
+
+
+
+    }
+}
