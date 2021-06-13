d399b6dc6ed9f2a89a177c529d063d155c92ed95
Rovshan Aliyev <raliyev831@yahoo.com>
2021-06-13 15:10:14 -0700

    day 52 mobile app example finished

--- src/day52_inheritance/Instagram.java
+++ src/day52_inheritance/Instagram.java
@@ -4,4 +4,11 @@ public class Instagram extends MobileApp {
     public void postPhoto() {
         System.out.println("Posting photo in Instagram");
     }
+    @Override
+    public void useTheApp(int minutes) {
+        super.useTheApp(minutes);
+        System.out.println("Using instagram app features");
+        postPhoto();
+    }
+
 }
