505970f1f9e7079073caed87427905d0f4ae25db
Rovshan Aliyev <raliyev831@yahoo.com>
2021-06-12 19:34:32 -0700

    day 52 mobile app example started

---
+++ src/day52_inheritance/Discord.java
@@ -0,0 +1,7 @@
+package day52_inheritance;
+
+public class Discord extends MobileApp{
+    public void chat(String someone) {
+        System.out.println("Chatting with " + someone + " on Discord");
+    }
+}
