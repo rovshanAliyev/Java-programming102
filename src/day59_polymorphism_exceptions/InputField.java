538277137101c9cc3217d4cfe0fb159d783f1342
Rovshan Aliyev <raliyev831@yahoo.com>
2021-07-04 13:18:24 -0700

    started day 59 practicing polymorphism

---
+++ src/day59_polymorphism_exceptions/InputField.java
@@ -0,0 +1,26 @@
+package day59_polymorphism_exceptions;
+
+public class InputField implements WebElement {
+    public static final String TAG_NAME = "input";
+
+    public String getValue() {
+        System.out.println("Getting value in the inputField");
+        return "selenium";
+    }
+
+    @Override
+    public void sendKeys(String text) {
+        System.out.println("Typing into input field - " + text );
+    }
+
+    @Override
+    public void click() {
+        System.out.println("");
+    }
+
+    @Override
+    public String getText() {
+        System.out.println("getting text of input field");
+        return "java";
+    }
+}
